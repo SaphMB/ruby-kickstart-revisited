96e3952a951f9550efc40f8bf49a2bff135ff95d
Sapphire Mason-Brown <saphmb@gmail.com>
2018-05-09 00:44:30 +0100

    Completed session 1

--- session1/challenge/6_string.rb
+++ session1/challenge/6_string.rb
@@ -1,11 +1,18 @@
 # You'll get a string and a boolean.
 # When the boolean is true, return a new string containing all the odd characters.
 # When the boolean is false, return a new string containing all the even characters.
-# 
+#
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
-# 
+#
 # odds_and_evens("abcdefg",true)    # => "bdf"
 # odds_and_evens("abcdefg",false)   # => "aceg"
 
 def odds_and_evens(string, return_odds)
+  return_string = ""
+  if return_odds == true
+    string.each_char.with_index {|char,index| return_string << char if index.even? == false}
+  else
+    string.each_char.with_index {|char,index| return_string << char if index.even? == true}
+  end
+  return return_string
 end
--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,11 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  return_string =  ""
+  string.each_char.with_index {|char,index| return_string << string[index+1] if index != string.length - 1 && char.downcase == "r"}
+  return return_string
 end
